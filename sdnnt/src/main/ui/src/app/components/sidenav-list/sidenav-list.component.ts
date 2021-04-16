2fed8194ce35b03b556a7bb7527cdc2f9d6865eb
Petr Kudela <kudy.samba@gmail.com>
2021-04-16 17:28:17 +0200

    navbar, sidenav list, html, scss

--- sdnnt/src/main/ui/src/app/components/sidenav-list/sidenav-list.component.ts
+++ sdnnt/src/main/ui/src/app/components/sidenav-list/sidenav-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { AppService } from 'src/app/app.service';
+import { AppState } from 'src/app/app.state';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -18,9 +20,17 @@ export class SidenavListComponent implements OnInit {
   // sidenav
   @Output() public sidenavToggle = new EventEmitter();
 
-  constructor(private overlay: Overlay) { }
+  constructor(
+    private overlay: Overlay,
+    private service: AppService,
+    public state: AppState
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  changeLang(lang: string) {
+    this.service.changeLang(lang);
   }
 
   // sidenav fuction
