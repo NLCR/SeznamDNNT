0f89f713ea5f7bf9d2e2578d400fdd0f874129c7
alberto <alberto.hernandez@inovatika.cz>
2021-07-28 12:30:06 +0200

    clear zadost on logout

--- sdnnt/src/main/ui/src/app/components/user-dialog/user-dialog.component.ts
+++ sdnnt/src/main/ui/src/app/components/user-dialog/user-dialog.component.ts
@@ -90,6 +90,7 @@ export class UserDialogComponent implements OnInit {
       this.state.setLogged(res);
       this.state.logged = false;
       this.state.user = null;
+      this.state.currentZadost = null;
       localStorage.removeItem('user');
       sessionStorage.clear();
       this.router.navigate(['/']);
