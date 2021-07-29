25e916fd815f8f5d86d469f32d67bf2f5d6cd8a0
pavel-stastny <pavel.stastny@gmail.com>
2021-07-29 11:41:59 +0200

    Multimodule pom, Date from introduced"w"wq

--- sdnnt/src/main/java/cz/inovatika/sdnnt/openapi/endpoints/api/impl/DNNTListApiServiceImpl.java
+++ sdnnt/src/main/java/cz/inovatika/sdnnt/openapi/endpoints/api/impl/DNNTListApiServiceImpl.java
@@ -3,6 +3,7 @@ package cz.inovatika.sdnnt.openapi.endpoints.api.impl;
 import cz.inovatika.sdnnt.index.CatalogSearcher;
 import cz.inovatika.sdnnt.openapi.endpoints.api.ListsApiService;
 import cz.inovatika.sdnnt.openapi.endpoints.api.NotFoundException;
+import org.apache.commons.collections4.iterators.ArrayListIterator;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FileUtils;
@@ -20,6 +21,10 @@ import javax.ws.rs.core.StreamingOutput;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -54,19 +59,30 @@ public class DNNTListApiServiceImpl extends ListsApiService {
 //        return Response.ok().entity(a.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
 //    }
 
+
+
     @Override
-    public Response listDnntoCsv(String instituion, SecurityContext securityContext, ContainerRequestContext containerRequestContext) throws NotFoundException {
-        List<String> plusList = (instituion != null) ?  Arrays.asList("marc_911a:"+instituion, "marc_990a:A") :Arrays.asList("marc_990a:A");
+    public Response listDnntoCsv(String instituion, OffsetDateTime dateTime, SecurityContext securityContext, ContainerRequestContext containerRequestContext) throws NotFoundException {
+        List<String> plusList = (instituion != null) ?  new ArrayList<>(Arrays.asList("marc_911a:"+instituion, "marc_990a:A")) :  new ArrayList<>(Arrays.asList("marc_990a:A"));
+        if (dateTime != null) {
+            String utc = DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.of("UTC")).format(dateTime.truncatedTo(ChronoUnit.MILLIS));
+            plusList.add("datum_stavu:["+utc+" TO *]");
+        }
+
         return csv(instituion,"dnnto",plusList, Arrays.asList("marc_990a:NZ"), Arrays.asList("nazev", "identifier", "marc_856u", "marc_990a", "marc_992s", "marc_911u", "marc_910a","marc_911a"));
     }
     @Override
-    public Response listDnnttCsv(String instituion, SecurityContext securityContext, ContainerRequestContext containerRequestContext) throws NotFoundException {
-        List<String> plusList = (instituion != null) ?  Arrays.asList("marc_911a:"+instituion, "marc_990a:A","marc_990a:NZ"): Arrays.asList("marc_990a:A","marc_990a:NZ");
+    public Response listDnnttCsv(String instituion, OffsetDateTime dateTime, SecurityContext securityContext, ContainerRequestContext containerRequestContext) throws NotFoundException {
+        List<String> plusList = (instituion != null) ?   new ArrayList<>(Arrays.asList("marc_911a:"+instituion, "marc_990a:A","marc_990a:NZ")):  new ArrayList<>(Arrays.asList("marc_990a:A","marc_990a:NZ"));
+        if (dateTime != null) {
+            String utc = DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.of("UTC")).format(dateTime.truncatedTo(ChronoUnit.MILLIS));
+            plusList.add("datum_stavu:["+utc+" TO *]");
+        }
         return csv(instituion,"dnntt",plusList, new ArrayList<>(), Arrays.asList("nazev", "identifier", "marc_856u", "marc_990a", "marc_992s", "marc_911u", "marc_910a","marc_911a"));
     }
 
 
-    private Response csv(String selectedInstitution, String label, List<String> plusList, List<String> minusList, List<String> fields) {
+    private Response csv(String selectedInstitution,String label, List<String> plusList, List<String> minusList, List<String> fields) {
         try {
             File csvFile = File.createTempFile("temp","csv");
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(csvFile), Charset.forName("UTF-8"));
@@ -79,7 +95,8 @@ public class DNNTListApiServiceImpl extends ListsApiService {
                     String identifier = (String) doc.getFieldValue("identifier");
 
                     Collection<Object> mlinks911u = doc.getFieldValues("marc_911u");
-                    Collection<Object> mlinks856u = null; //doc.getFieldValues("marc_856u");
+                    Collection<Object> mlinks856u =  doc.getFieldValues("marc_856u");
+                    Collection<Object> mlinks956u =  doc.getFieldValues("marc_956u");
 
                     // vsechny instituce, ktre to maji zdigitalizovane
                     //Collection<Object> minstitutions910a = doc.getFieldValues("marc_910a");
@@ -90,6 +107,8 @@ public class DNNTListApiServiceImpl extends ListsApiService {
                         mlinks911u.stream().map(Object::toString).forEach(links::add);
                     } else if (mlinks856u != null) {
                         mlinks856u.stream().map(Object::toString).forEach(links::add);
+                    } else if (mlinks956u != null) {
+                        mlinks956u.stream().map(Object::toString).forEach(links::add);
                     }
 
                     final List<String> institutions = new ArrayList<>();
