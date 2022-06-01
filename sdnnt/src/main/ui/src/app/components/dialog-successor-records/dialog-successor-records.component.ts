e951deeed175b247307b3f4bebc25e1d4691b070
Petr Kudela <kudy.samba@gmail.com>
2022-06-01 10:30:01 +0200

    new component created for "naslednicke zaznamy"

---
+++ sdnnt/src/main/ui/src/app/components/dialog-successor-records/dialog-successor-records.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-successor-records',
+  templateUrl: './dialog-successor-records.component.html',
+  styleUrls: ['./dialog-successor-records.component.scss']
+})
+export class DialogSuccessorRecordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
