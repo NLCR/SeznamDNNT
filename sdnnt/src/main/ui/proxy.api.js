5ca5325e539fbc5e6b1f9270a4e7f9c22595980e
Petr Kudela <kudy.samba@gmail.com>
2023-06-15 14:45:42 +0200

    proxy modifications

--- sdnnt/src/main/ui/proxy.api.js
+++ sdnnt/src/main/ui/proxy.api.js
@@ -1,6 +1,6 @@
 const PROXY_CONFIG = {
     "/api/**": {
-        "target": "http://localhost:8080/sdnnt/api",
+        "target": "http://localhost:18080/sdnnt/api",
         "changeOrigin": true,
         "secure": false,
         "logLevel": "debug",
@@ -18,7 +18,28 @@ const PROXY_CONFIG = {
 				console.log("Replaced cookie "+pr.headers['set-cookie']);
               }
         }
+   },
+   "/assets/**": {
+        "target": "http://localhost:18080/sdnnt/assets/",
+        "changeOrigin": true,
+        "secure": false,
+        "logLevel": "debug",
+		"pathRewrite": {
+            "^/assets":""
+    	},
+        "onProxyRes": function(pr, req, res) {
+			console.log("Intercepting request");	
+            if (pr.headers['set-cookie']) {
+				console.log("Replacing cookie "+pr.headers['set-cookie']);
+                const cookies = pr.headers['set-cookie'].map(cookie => 
+                    cookie.replace(/\/sdnnt/gi, '/')
+                );
+                pr.headers['set-cookie'] = cookies;
+				console.log("Replaced cookie "+pr.headers['set-cookie']);
+              }
+        }
    }
+
 };
 
 module.exports = PROXY_CONFIG;
