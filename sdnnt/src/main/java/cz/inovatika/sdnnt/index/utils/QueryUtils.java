2a0d1f24e7931f99b3bb5bba857e61c3534d75fc
pavel-stastny <pavel.stastny@gmail.com>
2023-07-10 09:32:12 +0200

    Issue #510 - Přidány patterny "19*", "20", "200", "201" , "20uu", "200u", "201u" t ish: q: command not found

--- sdnnt/src/main/java/cz/inovatika/sdnnt/index/utils/QueryUtils.java
+++ sdnnt/src/main/java/cz/inovatika/sdnnt/index/utils/QueryUtils.java
@@ -1,17 +1,23 @@
 package cz.inovatika.sdnnt.index.utils;
 
 import org.apache.solr.client.solrj.util.ClientUtils;
+import org.json.JSONArray;
 
 import cz.inovatika.sdnnt.Options;
 
 import java.text.BreakIterator;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class QueryUtils {
+    
+    public static final List<String> DEFAULT_QUERY_DATE_PATTERNS = Arrays.asList(
+            "19*", "20", "200", "201" , "20uu", "200u", "201u");
+    
 
     public static final String IDENTIFIER_PREFIX = "oai:aleph-nkp.cz:";
     
@@ -82,27 +88,59 @@ public class QueryUtils {
     /** Only lower bound filters; Issue #510 */
     public static String catalogSEFilterQueryPartOnlyLowerBound(Options opts, int year, int fromYear) {
         int yearsSE = opts.getJSONObject("search").getInt("yearsSE");
+
+        JSONArray patternsArray = opts.getJSONObject("search").optJSONArray("yearsSEPatterns");
+        List<Object> patterns = new ArrayList<>();
+
+        if (patternsArray != null) {
+            patternsArray.forEach(patterns::add);
+        } else {
+            DEFAULT_QUERY_DATE_PATTERNS.forEach(patterns::add);
+        }
+        
+        String date1Pattern = patterns.stream().map(it-> {
+            if (!it.toString().endsWith("*")) {
+                return '"'+it.toString()+'"';
+            } else return it.toString();
+        }).collect(Collectors.joining(" OR "));
+
         String seDate = "(date1_int:["
                 + fromYear
                 + " TO "
                 + (year - yearsSE)
-                +"])";
-    
+                +"] OR (NOT date1_int:* AND date1:("+date1Pattern+")))";
+        
         String se = "(fmt:SE AND " + seDate + ")";
         return se;
     }
 
     public static String catalogBKFilterQueryPartOnlyLowerBound(Options opts, int year, int fromYear) {
         int yearsBK = opts.getJSONObject("search").getInt("yearsBK");
+
+        List<Object> patterns = new ArrayList<>();
+
+        JSONArray patternsArray = opts.getJSONObject("search").optJSONArray("yearsBKPatterns" );
+        if (patternsArray != null) {
+            patternsArray.forEach(patterns::add);
+        } else {
+            DEFAULT_QUERY_DATE_PATTERNS.forEach(patterns::add);
+        }
+        
+        String date1Pattern = patterns.stream().map(it-> {
+            if (!it.toString().endsWith("*")) {
+                return '"'+it.toString()+'"';
+            } else return it.toString();
+        }).collect(Collectors.joining(" OR "));
+
         String bkDate = "(date1_int:["
                 + fromYear
                 + " TO "
                 + (year - yearsBK)
-                + "])";
+                + "] OR (NOT date1_int:* AND date1:("+date1Pattern+")))";
     
         String bk = "(fmt:BK AND " + bkDate + ")";
         return bk;
     }
     
-    
 }
+
--- sdnnt/src/test/java/cz/inovatika/sdnnt/utils/QueryUtilsTest.java
+++ sdnnt/src/test/java/cz/inovatika/sdnnt/utils/QueryUtilsTest.java
@@ -32,16 +32,17 @@ public class QueryUtilsTest {
         String catalogBKFilterQueryPart = QueryUtils.catalogBKFilterQueryPart(opts, 2023, 1911);
         Assert.assertTrue("(fmt:BK AND ((date1_int:[1911 TO 2012] AND -date2_int:*) OR (date1_int:[1911 TO 2012] AND date2_int:[1911 TO 2012])))".equals(catalogBKFilterQueryPart));
         
-        
         catalogBKFilterQueryPart = QueryUtils.catalogBKFilterQueryPartOnlyLowerBound(opts, 2023, 1911);
-        Assert.assertTrue("(fmt:BK AND (date1_int:[1911 TO 2012]))".equals(catalogBKFilterQueryPart));
+        //System.out.println("TEST "+catalogBKFilterQueryPart);
+        Assert.assertTrue("(fmt:BK AND (date1_int:[1911 TO 2012] OR (NOT date1_int:* AND date1:(19* OR \"20\" OR \"200\" OR \"201\" OR \"20uu\" OR \"200u\" OR \"201u\"))))".equals(catalogBKFilterQueryPart));
         
         
         String catalogSEFilterQueryPart = QueryUtils.catalogSEFilterQueryPart(opts, 2023, 1911);
         Assert.assertTrue("(fmt:SE AND ((date1_int:[1911 TO 2013] AND date2_int:9999) OR date2_int:[1911 TO 2013]))".equals(catalogSEFilterQueryPart));
 
         catalogSEFilterQueryPart = QueryUtils.catalogSEFilterQueryPartOnlyLowerBound(opts, 2023, 1911);
-        Assert.assertTrue("(fmt:SE AND (date1_int:[1911 TO 2013]))".equals(catalogSEFilterQueryPart));
+        //System.out.println(catalogSEFilterQueryPart);
+        Assert.assertTrue("(fmt:SE AND (date1_int:[1911 TO 2013] OR (NOT date1_int:* AND date1:(19* OR \"20\" OR \"200\" OR \"201\" OR \"20uu\" OR \"200u\" OR \"201u\"))))".equals(catalogSEFilterQueryPart));
     }
     
     
