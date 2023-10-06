9964e2a123752386d215d080776398a2b5011d13
pavel-stastny <pavel.stastny@gmail.com>
2023-10-06 11:05:35 +0200

    Shared exports.ts

---
+++ sdnnt/src/main/ui/src/app/shared/exports.ts
@@ -0,0 +1,12 @@
+export class Export {
+    id: string;
+    indextime: Date;
+    export_type: string;
+    export_processed: boolean;
+    export_num_docs: number;
+  }
+  
+
+export class ExportDoc {
+  
+}
